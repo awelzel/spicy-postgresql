a6643af66f6d1b1d855e4aa22f87baf5cf114ee8
Ret2c7 <1347405944@qq.com>
2023-09-21 16:53:37 +0800

    commit

---
+++ build/analyzer/CMakeFiles/PostgreSQL.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "../postgresql.hlto"
+  "CMakeFiles/PostgreSQL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/PostgreSQL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
